d743bfbc3f3cb5900b378c7228b60325a62ac7ff
ishikawaking <ishikawa.s.aj@m.titech.ac.jp>
2019-06-19 11:39:03 +0900

    む

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int sum = 0;
+    for(int j = 0;j < n;j++){
+        sum += A[j]/m;
+    }
+    return sum >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +18,14 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+    lb = 0;
+    ub = 1000000000;
+    while(ub - lb > 1){
+        int m = (lb + ub)/2;
+        if(p(m)){lb = m;}
+        else ub = m;
+    }
+    printf("%d\n", lb);
 
   return 0;
 }
