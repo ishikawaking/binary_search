d7b0ef54574a7a8f872b25091bf5a8d0d8f74549
ishikawaking <ishikawa.s.aj@m.titech.ac.jp>
2019-06-19 11:01:08 +0900

    ふ

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,13 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int sum = 0;
+    for(int j = 0;j < n;j++){
+        sum += (A[j] + m - 1)/m;
+    }
+    return sum <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -12,5 +19,13 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0;
+  ub = A[n-1];
+  while(ub - lb > 1){
+      int m = (lb + ub)/2;
+      if(p(m)){ub = m;}
+      else lb = m;
+  }
+  printf("%d\n", ub);
   return 0;
 }
